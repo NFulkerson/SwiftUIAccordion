d7d41f2cf66d7f2a17be1038dff7b462d9ca26a4
Nathan Fulkerson <4764329+NFulkerson@users.noreply.github.com>
2021-04-14 14:34:50 -0400

    Initial Commit

---
+++ SwiftUIAccordion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAccordion
+//
+//  Created by Nathan on 4/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAccordion/SwiftUIAccordionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAccordionApp.swift
+//  SwiftUIAccordion
+//
+//  Created by Nathan on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAccordionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
